676603d251b61f5316e13393652294e76f3b389e
Jeremy Banker <loredous@loredous.net>
2023-06-16 17:41:43 +0000

    Fix Python pep8 regressions in conftest.py
    Signed-off-by: Jeremy Banker <loredous@loredous.net>

--- bazel/python_utils/conftest.py
+++ bazel/python_utils/conftest.py
@@ -18,18 +18,18 @@ import pytest
 def pytest_sessionstart(session):
     if os.geteuid() != 0:
         raise Exception(
-            "\n\n" + \
-            "################################################################################\n" * 3 + \
-            "To execute tests tagged as 'manual' you need to use the relevant shell script in\n" + \
-            "'$MAGMA_ROOT/bazel/scripts/'!\n" + \
-            "For Python sudo tests: '$MAGMA_ROOT/bazel/scripts/run_sudo_tests.sh'!\n" + \
-            "For LTE integration tests: '$MAGMA_ROOT/bazel/scripts/run_integ_tests.sh'!\n" + \
-            "The scripts can be used to execute individual tests. To have the full overview\n" + \
-            "of the available options add '--help' to them.\n" + \
-            "Note: You got this error, because the test is tagged as manual and the user id\n" + \
-            "is not zero, which indicates that you are not running the test as root.\n" + \
-            "Disclaimer: Do not execute bazel commands as root, because it can destroy your\n" + \
-            "local bazel setup, you have to use the above mentioned scripts." + \
-            "\n################################################################################" * 3 + \
+            "\n\n" +
+            "################################################################################\n" * 3 +
+            "To execute tests tagged as 'manual' you need to use the relevant shell script in\n" +
+            "'$MAGMA_ROOT/bazel/scripts/'!\n" +
+            "For Python sudo tests: '$MAGMA_ROOT/bazel/scripts/run_sudo_tests.sh'!\n" +
+            "For LTE integration tests: '$MAGMA_ROOT/bazel/scripts/run_integ_tests.sh'!\n" +
+            "The scripts can be used to execute individual tests. To have the full overview\n" +
+            "of the available options add '--help' to them.\n" +
+            "Note: You got this error, because the test is tagged as manual and the user id\n" +
+            "is not zero, which indicates that you are not running the test as root.\n" +
+            "Disclaimer: Do not execute bazel commands as root, because it can destroy your\n" +
+            "local bazel setup, you have to use the above mentioned scripts." +
+            "\n################################################################################" * 3 +
             "\n\n",
         )
