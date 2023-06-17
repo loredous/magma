f389a2183d5b19fe2b9f14d9c07bc68b0b6ea0e0
Jeremy Banker <loredous@loredous.net>
2023-06-17 16:22:54 +0000

    Remove unneeded imports
    Signed-off-by: Jeremy Banker <loredous@loredous.net>

--- hil_testing/Magma_Automations/scripts/delete_tests.py
+++ hil_testing/Magma_Automations/scripts/delete_tests.py
@@ -13,10 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import json
 import logging
-import sys
-import warnings
 
 import requests
 from base import credentials
